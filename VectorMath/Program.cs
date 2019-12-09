711c951416efcac781463a43388943bf66767eef
ezajacthan <e>
2019-12-09 14:15:31 -0600

    Classwork 12-9

--- VectorMath/Program.cs
+++ VectorMath/Program.cs
@@ -7,6 +7,46 @@ namespace VectorMath
         static void Main(string[] args)
         {
             Console.WriteLine("Fun with vectors");
+
+            Vector3 one = new Vector3(5, 4, 3);
+            Vector3 two = new Vector3(2,8,6);
+
+            Console.WriteLine("Adding two vectors: ");
+            Console.WriteLine($"With a static method:\n {Vector3.Add(one, two)}");
+            one.Add(two);
+            Console.WriteLine($"With a class method:\n {one}");
+            one = new Vector3(5, 4, 3);
+            Console.WriteLine($"With operator overloading:\n {one + two}");
+
+            Console.WriteLine($"Vector subtraction (one - two):\n {one - two}");
+
+            Console.WriteLine($"Vector scaling (one * 10): \n {one * 10}");
+
+            Console.WriteLine($"Vector comparison of one and two:\n {one == two}");
+            Vector3 three = new Vector3(5, 4, 3);
+
+            Console.WriteLine($"Vector comparison of one and three:\n {one == three}");
+
+            if (one)
+            {
+                Console.WriteLine("Vector one is true and not zero");
+            }
+            else
+            {
+                Console.WriteLine("Vector one is zero and false");
+            }
+
+            Console.WriteLine("Vector one reassignment to <0,0,0>");
+            one = new Vector3(0, 0, 0);
+
+            if(one)
+            {
+                Console.WriteLine("Vector one is true and not zero");
+            }
+            else
+            {
+                Console.WriteLine("Vector one is zero and false");
+            }
         }
     }
 }
--- VectorMath/Vector3.cs
+++ VectorMath/Vector3.cs
@@ -18,11 +18,64 @@ namespace VectorMath
         }
 
         // Vector Addition
+        //functional way to add that returns the new vector result
+        public static Vector3 Add(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
+
+        //OOP way of adding that modifies the existing vector
+        public void Add(Vector3 b)
+        {
+            this.X += b.X;
+            this.Y += b.Y;
+            this.Z += b.Z;
+        }
+
+        //with operator overloading
+        public static Vector3 operator +(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+        }
 
         // Vector Subtraction 
+        public static Vector3 operator -(Vector3 a, Vector3 b)
+        {
+            return new Vector3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+        }
 
         // Vector Scaling 
+        public static Vector3 operator *(Vector3 a, double s)
+        {
+            return new Vector3(a.X * s, a.Y * s, a.Z * s);
+        }
 
         // Vector Comparison
+        public static bool operator ==(Vector3 a, Vector3 b)
+        {
+            return (a.X == b.X && a.Y == b.Y && a.Z == b.Z);
+        }
+        public static bool operator !=(Vector3 a, Vector3 b)
+        {
+            return a.X != b.X || a.Y != b.Y || a.Z != b.Z;
+        }
+
+        public override string ToString()
+        {
+            return $"X: {this.X} \n Y: {this.Y} \n Z: {this.Z}";
+        }
+
+        //Other operator overloading
+        public static readonly Vector3 Zero = new Vector3(0, 0, 0);
+
+        public static bool operator true(Vector3 a)
+        {
+            return Vector3.Zero != a;
+        }
+        public static bool operator false(Vector3 a)
+        {
+            return Vector3.Zero == a;
+        }
+
     }
 }
